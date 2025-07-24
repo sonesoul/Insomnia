2af95b5bf7caebd25c0b1ec1a85098d8672d69c2
sonesoul <smippy098@gmail.com>
2025-07-24 20:41:09 +0300

    replace InputSimulator dependency with manual WinAPI invocation and change .NET Framework version 9.0 -> 6.0

--- AwakeKeeper.cs
+++ AwakeKeeper.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Drawing.Design;
-using System.Numerics;
-using WindowsInput;
 
 namespace Insomnia
 {
-    public class AwakeKeeper() : MouseSimulator(new InputSimulator())
+    public class AwakeKeeper
     {
         public Point MousePosition { get; private set; } = new();
         public int OffsetPx { get; set; } = 10; 
@@ -13,14 +10,12 @@ namespace Insomnia
         public float MoveDelaySeconds { get; set; } = 1f;
 
         private bool _toggle = true;
-        private float _activityElapsed = 0;
-        private float _moveElapsed = 0;
-
+        
         public void Update()
         {
             if (NativeInterop.GetIdleTime() >= IdleThreshold)
             {
-                MoveMouseBy((_toggle = !_toggle) ? -OffsetPx : OffsetPx, 0);
+                NativeInterop.MoveMouseBy((_toggle = !_toggle) ? -OffsetPx : OffsetPx, 0);
             }
         }
     }
--- NativeInterop.cs
+++ NativeInterop.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing.Text;
 using System.Runtime.InteropServices;
 
 namespace Insomnia
@@ -12,13 +13,41 @@ namespace Insomnia
             public uint dwTime;
         }
 
+        [StructLayout(LayoutKind.Sequential)]
+        struct INPUT
+        {
+            public uint type;
+            public InputUnion U;
+        }
+        
+        [StructLayout(LayoutKind.Explicit)]
+        struct InputUnion
+        {
+            [FieldOffset(0)] public MOUSEINPUT mi;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+
         public const int SW_HIDE = 0;
         public const int SW_SHOW = 5;
+        public const uint INPUT_MOUSE = 0;
+        public const uint MOUSEEVENTF_MOVE = 0x0001;
 
         [DllImport("user32.dll")]
         private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
         [DllImport("user32.dll")]
         public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
 
         public static TimeSpan GetIdleTime()
         {
@@ -29,6 +58,28 @@ namespace Insomnia
             uint idleTicks = ((uint)Environment.TickCount - info.dwTime);
             return TimeSpan.FromMilliseconds(idleTicks);
         }
-        
+
+        public static void MoveMouseBy(int deltaX, int deltaY)
+        {
+            INPUT input = new()
+            {
+                type = INPUT_MOUSE,
+                U = new InputUnion
+                {
+                    mi = new MOUSEINPUT
+                    {
+                        dx = deltaX,
+                        dy = deltaY,
+                        mouseData = 0,
+                        dwFlags = MOUSEEVENTF_MOVE,
+                        time = 0,
+                        dwExtraInfo = IntPtr.Zero
+                    }
+                }
+            };
+
+            //handling HResult in process
+            uint HResult = SendInput(1, new INPUT[] { input }, Marshal.SizeOf<INPUT>());
+        }
     }
 }
