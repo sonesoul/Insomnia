5cfa3cb556dc038825bd9efe874b3102985d640b
sonesoul <smippy098@gmail.com>
2025-07-30 00:22:22 +0300

    add enable/disable function

--- AwakeKeeper.cs
+++ AwakeKeeper.cs
@@ -8,11 +8,15 @@ namespace Insomnia
         public int OffsetPx { get; set; } = 10; 
         public TimeSpan IdleThreshold { get; set; } = TimeSpan.FromSeconds(1);
         public float MoveDelaySeconds { get; set; } = 1f;
+        public bool Enabled { get; set; } = true;
 
         private bool _toggle = true;
         
         public void Update()
         {
+            if (!Enabled)
+                return;
+
             if (NativeInterop.GetIdleTime() >= IdleThreshold)
             {
                 NativeInterop.MoveMouseBy((_toggle = !_toggle) ? -OffsetPx : OffsetPx, 0);
