c2c3598ccd6fbd2283c35ce403db9ef54031f1be
sonesoul <smippy098@gmail.com>
2025-08-05 17:52:28 +0300

    add SDL.Color support to RGB and change to RGBA

--- Assets/Palette.cs
+++ Assets/Palette.cs
@@ -1,27 +1,25 @@
 ﻿namespace Insomnia.Assets
 {
-    
-
     public class Palette
     {
-        public static RGB Black => Colors[0];
-        public static RGB DarkBlue => Colors[1];
-        public static RGB DarkPurple => Colors[2];
-        public static RGB DarkGreen => Colors[3];
-        public static RGB Brown => Colors[4];
-        public static RGB DarkGray => Colors[5];
-        public static RGB LightGray => Colors[6];
-        public static RGB White => Colors[7];
-        public static RGB Red => Colors[8];
-        public static RGB Orange => Colors[9];
-        public static RGB Yellow => Colors[10];
-        public static RGB Green => Colors[11];
-        public static RGB Blue => Colors[12];
-        public static RGB Indigo => Colors[13];
-        public static RGB Pink => Colors[14];
-        public static RGB Peach => Colors[15];
+        public static RGBA Black => Colors[0];
+        public static RGBA DarkBlue => Colors[1];
+        public static RGBA DarkPurple => Colors[2];
+        public static RGBA DarkGreen => Colors[3];
+        public static RGBA Brown => Colors[4];
+        public static RGBA DarkGray => Colors[5];
+        public static RGBA LightGray => Colors[6];
+        public static RGBA White => Colors[7];
+        public static RGBA Red => Colors[8];
+        public static RGBA Orange => Colors[9];
+        public static RGBA Yellow => Colors[10];
+        public static RGBA Green => Colors[11];
+        public static RGBA Blue => Colors[12];
+        public static RGBA Indigo => Colors[13];
+        public static RGBA Pink => Colors[14];
+        public static RGBA Peach => Colors[15];
 
-        private static readonly RGB[] Colors = new RGB[]
+        private static readonly RGBA[] Colors = new RGBA[]
         {
             new(0, 0, 0),         // Black
             new(29, 43, 83),      // Dark Blue
---
+++ Assets/RGBA.cs
@@ -0,0 +1,20 @@
+﻿using DrawingColor = System.Drawing.Color;
+using SDLColor = SDL3.SDL.Color;
+
+namespace Insomnia.Assets
+{
+    public struct RGBA(byte r, byte g, byte b, byte a)
+    {
+        public byte Red = r;
+        public byte Green = g;
+        public byte Blue = b;
+        public byte Alpha = a;
+
+        public RGBA(byte r, byte g, byte b) : this(r, g, b, byte.MaxValue)
+        {
+        }
+
+        public static implicit operator DrawingColor(RGBA rgba) => DrawingColor.FromArgb(rgba.Alpha, rgba.Red, rgba.Green, rgba.Blue);
+        public static implicit operator SDLColor(RGBA rgba) => new() { R = rgba.Red, G = rgba.Green, B = rgba.Blue, A = rgba.Alpha };
+    }
+}
