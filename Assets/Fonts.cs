2ad85c353e9ba2e86ddb33df0063836f55973610
sonesoul <smippy098@gmail.com>
2025-08-15 16:29:57 +0300

    add global font set

---
+++ Assets/Fonts.cs
@@ -0,0 +1,17 @@
+﻿using Insomnia.DirectMedia.Types;
+using System;
+
+namespace Insomnia.Assets
+{
+    public static class Fonts
+    {
+        public static Font Pico8Mono => FontCreators[0]();
+
+        private static Func<Font>[] FontCreators { get; } = 
+        [
+            () => Load("pico-8-mono.ttf", 8),
+        ];
+
+        private static Font Load(string relativePath, float size) => new(Asset.GetAbsolutePath(relativePath), size);
+    }
+}
