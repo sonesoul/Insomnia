0862c7e20008055af18e6629db194922b77d3575
sonesoul <smippy098@gmail.com>
2025-08-15 16:41:11 +0300

    add global icon set

---
+++ Assets/Icons.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+
+namespace Insomnia.Assets
+{
+    public static class Icons
+    {
+        public static Icon Active => IconCreators[0]();
+        public static Icon Inactive => IconCreators[1]();
+
+        private static Func<Icon>[] IconCreators { get; } = 
+        [
+            () => Load("Icon_Active.ico"),
+            () => Load("Icon_Inactive.ico"),
+        ];
+
+        private static Icon Load(string relativePath) => new Icon(Asset.GetAbsolutePath(relativePath));
+    }
+}
